d22239f370098047311bb1fa2d67c502db9fdd2d
marcoantonnlopez <a305350@alumnos.uaslp.mx>
2023-11-24 14:06:15 -0600

    fix(navbar): navbar functionality and design

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   logoNavbar: string = ''; 
+  isScrolled = false;
 
   ngOnInit(): void {
     this.logoNavbar = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1700630770/share/studevsLogo_r1s9jl.svg';
   }
   
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isScrolled = scrollPosition > 100; // Ajusta el 100 según tus necesidades
+  }
 }
