88144020dbab1175e938ffee6e468710a1abc81c
marcoantonnlopez <a305350@alumnos.uaslp.mx>
2023-11-29 00:21:02 -0600

    feat(aboutUs): secSeven

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -11,11 +11,23 @@ export class AboutUsComponent implements OnInit  {
   capi1: string = ''; 
   studevsLogo: string = ''; 
   scale: number = 1; // Define la propiedad scale
+  sec7_1: string = ''; 
+  sec7_2: string = ''; 
+  sec7_3: string = ''; 
+  sec7_4: string = ''; 
+  sec7_5: string = ''; 
+  sec7_6: string = ''; 
 
   ngOnInit(): void {
     this.aboutUsLetter = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701140218/AboutUs/AboutUs_Letters_aerg4p.svg';
     this.capi1 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701213737/capi_yf5t4l.svg';
     this.studevsLogo = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1700630770/share/LogoNormal_ojrguh.svg';
+    this.sec7_1 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701237291/AboutUs/se7_1_bylluf.png';
+    this.sec7_2 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701237294/AboutUs/se7_2_i6kmko.png';
+    this.sec7_3 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701237214/AboutUs/se7_3_zrlllc.png';
+    this.sec7_4 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701237216/AboutUs/se7_4_erdvyb.png';
+    this.sec7_5 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701237215/AboutUs/se7_5_idikyf.png';
+    this.sec7_6 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701238597/AboutUs/se7_61_jrpbn4.png';
   }
 
   @HostListener('window:scroll', ['$event'])
