bda731b60b08ad3ffc597e5a854ad9595d8e1f9c
marcoantonnlopez <a305350@alumnos.uaslp.mx>
2023-11-29 11:41:43 -0600

    fix error

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -17,6 +17,7 @@ export class AboutUsComponent implements OnInit  {
   sec7_4: string = ''; 
   sec7_5: string = ''; 
   sec7_6: string = ''; 
+  capi2: string = ''; 
 
   ngOnInit(): void {
     this.aboutUsLetter = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701140218/AboutUs/AboutUs_Letters_aerg4p.svg';
@@ -28,6 +29,7 @@ export class AboutUsComponent implements OnInit  {
     this.sec7_4 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701237216/AboutUs/se7_4_erdvyb.png';
     this.sec7_5 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701237215/AboutUs/se7_5_idikyf.png';
     this.sec7_6 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701238597/AboutUs/se7_61_jrpbn4.png';
+    this.capi2 = 'https://res.cloudinary.com/dmhkvcej4/image/upload/v1701213722/capiFinal_racltk.svg';
   }
 
   @HostListener('window:scroll', ['$event'])
