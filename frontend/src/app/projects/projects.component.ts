3c2b79b26e563c80a271d08f0648df9db670a7c8
marcoantonnlopez <a305350@alumnos.uaslp.mx>
2023-12-04 14:08:02 -0600

    feat/projects&members/addFrontMainView

--- frontend/src/app/projects/projects.component.ts
+++ frontend/src/app/projects/projects.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0 })),
+      state('*', style({ opacity: 1 })),
+      transition(':enter', [
+        animate('1s ease-in')
+      ])
+    ])
+  ]
 })
 export class ProjectsComponent {
+  miRuta: string = '/user';
+  miTexto: string = '+ Add a project';
 
 }
